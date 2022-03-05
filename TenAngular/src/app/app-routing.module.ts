2a4fef1adcfa7c52d15ae3a4f7135963aef4fb53
Robert Nițu <niturobert02@yahoo.com>
2022-03-05 15:41:10 +0200

    Added home-page and notFound-page routes

--- TenAngular/src/app/app-routing.module.ts
+++ TenAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TenAngular/src/app/app.module.ts
+++ TenAngular/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
