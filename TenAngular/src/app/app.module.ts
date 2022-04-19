e6638caed77f8ee8b1178fd778d97f5bfe8f0cbc
Robert Nițu <niturobert02@yahoo.com>
2022-04-19 20:59:15 +0300

    Added tic-tac-toe component.

--- TenAngular/src/app/app.module.ts
+++ TenAngular/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatChipsModule} from "@angular/material/chips";
+import { MatChipsModule } from '@angular/material/chips';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TicTacToePageComponent } from './tic-tac-toe-page/tic-tac-toe-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundPageComponent, HomePageComponent],
+  declarations: [
+    AppComponent,
+    NotFoundPageComponent,
+    HomePageComponent,
+    TicTacToePageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
